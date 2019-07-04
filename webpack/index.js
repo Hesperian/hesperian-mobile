ae05c6c61926feee65dd549c4591beff5a535bd7
Matthew Litwin <mlitwin@sonic.net>
2019-07-04 16:16:50 -0700

    refactor webpack entrypoint

---
+++ webpack/index.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+
+function createConfig(spec) {
+  const preProcess = spec.preProcess;
+  const appConfig = spec.appConfig;
+  const rootDir = spec.rootDir;
+
+  let assets = [{
+      from: 'img/**/*',
+      to: '.'
+    },
+    {
+      from: 'locales/**/*',
+      to: '.'
+    }
+  ];
+
+  if (spec.addtionalAssets) {
+    assets = assets.concat(spec.addtionalAssets);
+  }
+
+
+  const copyWebpackPlugin = new CopyWebpackPlugin(assets, {});
+
+  const config = {
+    mode: 'development',
+    context: path.resolve(rootDir, 'www'),
+    entry: './js/app.js',
+    plugins: [
+      new webpack.DefinePlugin({
+        __VERSION__: JSON.stringify(appConfig.version),
+        __PREPROCESS__: JSON.stringify(preProcess.getPageInfo())
+      }),
+      new HtmlWebpackPlugin({
+        title: appConfig.description,
+        template: 'index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+      copyWebpackPlugin
+    ],
+    module: {
+      rules: [{
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env',
+                  {
+                    debug: true,
+                    "useBuiltIns": "entry",
+                    "corejs": {
+                      "version": 3,
+                      "proposals": true
+                    },
+                    "targets": {
+                      "android": "4.4",
+                      "ios": "9"
+                    }
+                  }
+                ]
+              ]
+            }
+          }
+        },
+        {
+          test: /\.(s*)css$/,
+          use: [{
+            loader: MiniCssExtractPlugin.loader
+          }, 'css-loader', {
+            loader: "sass-loader",
+            options: {
+              includePaths: ['./www/css']
+            }
+          }]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf)$/,
+          loader: 'url-loader?limit=100000'
+        }
+      ]
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(rootDir, 'dist')
+    }
+  };
+
+  return config;
+}
+module.exports = {
+  createConfig: createConfig
+};
